78e1ea4ca21fc19103828a3f1072f40bafbfcbb6
Shankaranarayanansk <sankaranarayananit002@gmail.com>
2024-12-29 19:07:48 +0530

    Admin layout done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,11 @@ import { Route, Routes } from "react-router-dom";
 import AuthLayout from "./components/auth/layout";
 import AuthLogin from "./Pages/auth/login";
 import AuthRegister from "./Pages/auth/Register";
+import AdminLayout from "./components/admin/layout";
+import AdminDashboard from "./Pages/AdminPanel/Dashboard";
+import AdminOrder from "./Pages/AdminPanel/orders";
+import AdminProduct from "./Pages/AdminPanel/products";
+import AdminFeature from "./Pages/AdminPanel/feature";
 
 const App = () => {
   return (
@@ -12,6 +17,12 @@ const App = () => {
           <Route path="login" element={<AuthLogin />} />
           <Route path="register" element={<AuthRegister />} />
         </Route>
+          <Route path="/admin" element={<AdminLayout/>}>
+            <Route path="/dashboard" element={<AdminDashboard/>}/>
+            <Route path="/orders" element={<AdminOrder/>}/>
+            <Route path="/products" element={<AdminProduct/>}/>
+            <Route path="/features" element={<AdminFeature/>}/>
+          </Route>
       </Routes>
     </div>
   );
